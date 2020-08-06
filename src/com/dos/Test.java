4a16e243acc7d6db105e6062d18b7e25a697f59f
xiaohuihui <wxp19970403>
2020-08-06 19:08:12 +0800

    lll

---
+++ src/com/dos/Test.java
@@ -0,0 +1,15 @@
+package com.dos;
+
+public class Test {
+    public static void main(String[] args) {
+        String s="90090";
+        Integer s3=null;
+        System.out.println(s);
+        System.out.println(s3);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s3);
+        System.out.println(s);
+        };
+    }
+
